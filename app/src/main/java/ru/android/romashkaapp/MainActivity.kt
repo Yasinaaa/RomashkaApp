01915ba8cc20ed6bfeb346ff37f844f5e8ed0ad1
Zalyalova Yasina <zalyalova@infomatika.ru>
2020-06-16 11:43:46 +0300

    Start creating documentation for API methods

--- app/src/main/java/ru/android/romashkaapp/MainActivity.kt
+++ app/src/main/java/ru/android/romashkaapp/MainActivity.kt
@@ -1,13 +1,12 @@
 package android.ru.romashkaapp
 
-import android.graphics.PointF
 import android.os.Bundle
-import android.util.Log
-import android.view.GestureDetector
-import android.view.GestureDetector.SimpleOnGestureListener
-import android.view.MotionEvent
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,14 +14,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         imageView.setImageDrawable(getDrawable(R.drawable.ic_plan))
+//        wb.setWebViewClient(MyBrowser())
+//        wb.getSettings().setLoadsImagesAutomatically(true)
+//        wb.getSettings().setJavaScriptEnabled(true)
+//        wb.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY)
+//        wb.loadUrl("https://demo-games.infomatika.ru/ru/")
 
-        val gestureDetector =
-            GestureDetector(this, object : SimpleOnGestureListener() {
-                override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
-                    Log.d("adaa", " " + e.x + " " + e.y)
-                    return true
-                }
-            })
-        imageView.setOnTouchListener { view, motionEvent -> gestureDetector.onTouchEvent(motionEvent) }
+//        val gestureDetector =
+//            GestureDetector(this, object : SimpleOnGestureListener() {
+//                override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
+//                    Log.d("adaa", " " + e.x + " " + e.y)
+//                    return true
+//                }
+//            })
+//        imageView.setOnTouchListener { _, motionEvent -> gestureDetector.onTouchEvent(motionEvent) }
+    }
+
+    private class MyBrowser : WebViewClient() {
+        override fun shouldOverrideUrlLoading(
+            view: WebView,
+            url: String
+        ): Boolean {
+            view.loadUrl(url)
+            return true
+        }
+
+        override fun onPageFinished(view: WebView, url: String) {
+
+        }
     }
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Документация
+##(API методы для игрового приложения по мнению Ясины)
+[https://docs.google.com/document/d/1RoM10khaV6lVp6d_qOKG_kk66p9ptz5zkd2r7nHSPg0/edit?usp=sharing]
