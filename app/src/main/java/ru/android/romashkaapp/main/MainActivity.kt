922e3844fa2eece2907d7182deb4e8657277b07a
Yasina Zalyalova <zalyalovayasina@gmail.com>
2020-10-16 11:07:27 +0300

    Solved problem with svg points clicks

--- app/src/main/java/ru/android/romashkaapp/main/MainActivity.kt
+++ app/src/main/java/ru/android/romashkaapp/main/MainActivity.kt
@@ -1,56 +1,65 @@
 package ru.android.romashkaapp.main
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import ru.android.romashkaapp.R
 
 
 class MainActivity : AppCompatActivity() {
 
-    /**
-     * You client id, you have it from the google console when you register your project
-     * https://console.developers.google.com/a
-     */
-    private val CLIENT_ID =
-        "1020597890643-n3m1t7fplcv2t0f78g7miachq7lgnbrv.apps.googleusercontent.com"
-
-    /**
-     * The redirect uri you have define in your google console for your project
-     */
-    private val REDIRECT_URI = "ru.android.romashkaapp:/sdd"
-
-    /**
-     * The redirect root uri you have define in your google console for your project
-     * It is also the scheme your Main Activity will react
-     */
-    private val REDIRECT_URI_ROOT = "ru.android.romashkaapp"
-
-    /**
-     * You are asking to use a code when autorizing
-     */
-    private val CODE = "code"
-
-    /**
-     * The scope: what do we want to use
-     * Here we want to be able to do anything on the user's GDrive
-     */
-    val API_SCOPE = "https://www.googleapis.com/auth/drive"
-
     private var viewModel: MainViewModel? = null
 
-    @SuppressLint("SetJavaScriptEnabled")
+    object AndroidJSInterface {
+        @JavascriptInterface
+        fun onClicked(id: String?) {
+            Log.d("TAG12345", "Clicked id=$id")
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                loadJs(view)
+            }
+        }
+        val webView = WebView(this)
+        webView.webViewClient = webViewClient
+        webView.settings.javaScriptEnabled = true
+        webView.settings.builtInZoomControls = true;
+        webView.addJavascriptInterface(AndroidJSInterface, "Android")
+        setContentView(webView)
+        webView.loadUrl("file:///android_asset/www/svgsina.svg")
+    }
+    private fun loadJs(webView: WebView) {
+//        webView.loadUrl(
+//            """javascript:(function f() {
+//                var b = document.getElementById('namefromjs')
+//                b.setAttribute('onclick', 'Android.onClicked()');})()"""
+//        )
+        webView.loadUrl(
+            """javascript:(function f() {
+    var sectors = document.querySelectorAll('g[free][view_id]');
+    for (const sector of sectors) {
+      var sectorId = sector.getAttribute('sector_id');
+      sector.setAttribute('onclick', 'Android.onClicked(' + sectorId + ')');
+    }
+  })()"""
+        )
+        webView.requestFocus()
+    }
+
+//    @SuppressLint("SetJavaScriptEnabled", "JavascriptInterface")
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
 //        imageView.setImageDrawable(getDrawable(R.drawable.ic_plan))
 
 //        val factory: ViewModelProvider.Factory = NewInstanceFactory()
@@ -88,42 +97,45 @@ class MainActivity : AppCompatActivity() {
 //        }
 //
 //        webview.webViewClient = webViewClient
-        webview.settings.javaScriptEnabled = true
-        webview.settings.userAgentString = "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)"
-        webview.addJavascriptInterface(AndroidJSInterface(), "HTMLOUT")
-        webview.webChromeClient = MyChromeClient()
+//        webview.settings.javaScriptEnabled = true
+//        webview.settings.userAgentString = "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)"
+//        webview.addJavascriptInterface(MyJavaScriptInterface(), "HTMLOUT")
+//        webview.webChromeClient = MyChromeClient()
+//        webview.loadUrl("file:///android_asset/www/index.html");
+////        webview.loadUrl("https://demo-games.infomatika.ru/ru/site/htmlsina")
+//        webview.requestFocus()
+//
+////        button.setOnClickListener{webview.loadUrl(
+////            "javascript:(function(){"+
+////                    "l=document.getElementById('namefromjs');"+
+////                    "e=document.createEvent('HTMLEvents');"+
+////                    "e.initEvent('click',true,true);"+
+////                    "l.dispatchEvent(e);"+
+////                    "})()"
+////        )}
+//        buttonClick.setOnClickListener { webview.loadUrl("javascript:(function(){document.getElementById('buttonClick').click();})()");}
+//    }
 
-        webview.loadUrl("https://demo-games.infomatika.ru/ru/site/htmlsina")
-        webview.requestFocus()
-
-        button.setOnClickListener{webview.loadUrl(
-            "javascript:(function(){"+
-                    "l=document.getElementById('namefromjs');"+
-                    "e=document.createEvent('HTMLEvents');"+
-                    "e.initEvent('click',true,true);"+
-                    "l.dispatchEvent(e);"+
-                    "})()"
-        )}
-        buttonClick.setOnClickListener { webview.loadUrl("javascript:(function(){document.getElementById('buttonClick').click();})()") }
-    }
-
-    class AndroidJSInterface {
-
-        @JavascriptInterface
-        fun onClicked(sectorId: String) {
-            Log.d("TAG", "Help button clicked")
-        }
-        @SuppressWarnings("unused")
+    class MyJavaScriptInterface {
         fun showHTML(html: String?) {
             Log.d("TAG", "Help button clicked")
         }
     }
 
+//    class AndroidJSInterface {
+//
+//        @JavascriptInterface
+//        fun onClicked(sectorId: String) {
+//            Log.d("TAG", "Help button clicked")
+//        }
+//        @SuppressWarnings("unused")
+//        fun showHTML(html: String?) {
+//            Log.d("TAG", "Help button clicked")
+//        }
+//    }
+
     internal class MyChromeClient : WebChromeClient()
 
-    private fun loadJs(webView: WebView) {
-
-    }
 
     private class MyBrowser : WebViewClient() {
         override fun shouldOverrideUrlLoading(
